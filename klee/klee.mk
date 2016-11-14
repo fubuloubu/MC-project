ab97e9c4aa527cc233f2133f4eb8cb0d981e4981
Bryant Eisenbach <fubuloubu@gmail.com>
2016-11-13 23:18:30 -0500

    Significantly modified rules

--- klee/klee.mk
+++ klee/klee.mk
@@ -4,34 +4,55 @@ IGNORE=/dev/null
 DOCKER=docker
 START_DIR=$(shell pwd)
 TARGET_DIR=/home/klee/test
-CONTAINER_NAME=klee_test_env
+CONTAINER_NAME=klee_test
 
-QUERY_ENV=$(DOCKER) ps -a | grep -o $(CONTAINER_NAME)
-CREATE_ENV=$(DOCKER) run -v $(START_DIR):$(TARGET_DIR) -it -d --name=$(CONTAINER_NAME) \
-	klee/klee 2>$(IGNORE) >$(IGNORE)
-ENV_EXECUTE=$(DOCKER) exec $(CONTAINER_NAME)
-CLEANUP_ENV=$(DOCKER) stop $(CONTAINER_NAME) 2>$(IGNORE) >$(IGNORE)\
-	    && $(DOCKER) rm $(CONTAINER_NAME) 2>$(IGNORE) >$(IGNORE)
-
-%.bc: %.c
-ifneq ($(shell $(QUERY_ENV)),$(CONTAINER_NAME))
+# Rule for creating environment (NOTE: keep persistant)
+.SECONDARY: $(CONTAINER_NAME)
+$(CONTAINER_NAME):
 	@echo " CREATE $(CONTAINER_NAME)"
-	@$(CREATE_ENV)
-endif
-	@echo " CLANG $^"
-	@$(ENV_EXECUTE) clang -emit-llvm -g -c $(TARGET_DIR)/$^ -o $@
-	@$(ENV_EXECUTE) mv $@ $(TARGET_DIR)
+	@$(DOCKER) run -v $(START_DIR):$(TARGET_DIR) \
+		-it -d --name=$(CONTAINER_NAME) \
+		klee/klee 2>$(IGNORE) >$(IGNORE)
+	@touch $@
 
-%.klee: %.bc
-	@echo "  KLEE $^"
-	@$(ENV_EXECUTE) klee --libc=uclibc --posix-runtime $(TARGET_DIR)/$^ 2>$(IGNORE)
+# Command to execute inside environment
+ENV_EXECUTE=$(DOCKER) exec $(CONTAINER_NAME)
 
-# NOTE: Cannot execute clean and above rules at the same time due to conditionals
+# Setup clang to generate bytecode for use with klee
+CLANG_FLAGS =-c
+CLANG_FLAGS+=-g
+CLANG_FLAGS+=-emit-llvm
+CLANG_FLAGS+=-I /home/klee/klee_src/include/
+CLANG=clang $(CLANG_FLAGS)
+
+# Generate LLVM bytecode for use with klee
+%.bc: %.c $(CONTAINER_NAME)
+	@echo "  CLANG $<"
+	@$(ENV_EXECUTE) $(CLANG) $(TARGET_DIR)/$< -o $(TARGET_DIR)/$@
+
+# Setup klee to generate test cases
+KLEE=klee --libc=uclibc --posix-runtime
+
+# Run klee to get test cases and execute them
+%.results: %.bc $(CONTAINER_NAME)
+	@echo "   KLEE $<"
+	@$(ENV_EXECUTE) $(KLEE) $(TARGET_DIR)/$< 2>$(IGNORE)
+	@cd klee-out-0 && for file in test*.ktest; do \
+		$(ENV_EXECUTE) ktest-tool $(TARGET_DIR)/klee-last/$$file >> $@; \
+	 done
+	@mv klee-out-0/$@ $@
+
+# Remove environment
+destroy_env:
+	@echo "DESTROY $(CONTAINER_NAME)"
+	@$(DOCKER) stop $(CONTAINER_NAME) 2>$(IGNORE) >$(IGNORE)
+	@$(DOCKER) rm   $(CONTAINER_NAME) 2>$(IGNORE) >$(IGNORE)
+	@rm $(CONTAINER_NAME)
+
+# Clean rule:
+# clean using executeable lines of .gitignore file
 .PHONY: clean
 clean:
-ifeq ($(shell $(QUERY_ENV)),$(CONTAINER_NAME))
-	@echo "DESTROY $(CONTAINER_NAME)"
-	@$(CLEANUP_ENV)
-endif
-	@echo " CLEAN klee/.gitignore"
+	@if [ -f $(CONTAINER_NAME) ]; then $(MAKE) -s destroy_env; fi
+	@echo "  CLEAN klee/.gitignore"
 	@grep -v "#\|^\$$" .gitignore | while read line; do rm -rf $$line; done
